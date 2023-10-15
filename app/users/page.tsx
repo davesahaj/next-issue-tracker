a82561c390fab134bc0b7bd9e4c0e52669ef1ae1
Dave <sahajdavework@pm.me>
2023-10-15 09:39:31 +0530

    added users list

---
+++ app/users/page.tsx
@@ -0,0 +1,24 @@
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map(({ id, name }) => (
+          <li key={id}>{name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
