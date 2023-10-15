693f3261f232019beaa2831727ad488fe6295c3c
Dave <sahajdavework@pm.me>
2023-10-15 09:12:29 +0530

    added components

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => 0}>hello</button>
+    </div>
+  );
+};
+
+export default AddToCart;
